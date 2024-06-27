a714eee2c7d8b4e01619d70577e9cfb101e3d4e5
Ann <ann20021007@gmail.com>
2024-06-27 18:05:02 +0800

    Add the quantization sh file

---
+++ model_API2_model7/code/quantize/run_quantize_use_customized_data_test.sh
@@ -0,0 +1,13 @@
+cd ../com/ 
+
+python quantize_finetune_model.py \
+  --model ../../float/model_7.pth \
+  --quantized_model_name my_quantized_model_7.pth \
+  --quant_mode test\
+  --batchsize 1 \
+  --quantize True \
+  --quantize_output_dir ../../quantized/ \
+  --finetune True \
+  --deploy True \
+  --train_dataset_path ../../data/My_dataset/train_spec_LATrain_audio_shuffle23_NOT_preprocessing \
+  --val_dataset_path ../../data/My_dataset/valid_spec_LATrain_audio_shuffle4_NOT_preprocessing \
